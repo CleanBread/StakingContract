46d1a66bc9cdd613c31dc6a22e1dfcb0fac3c806
Gleb Chistyakov <gleb4ist@gmail.com>
2021-11-21 20:01:59 +0300

    unstake func, tests

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IERC20Rewards.sol";
 
-contract Staking {
+contract Staking is Ownable {
     IERC20Rewards private ERC20Rewards;
 
     struct Stake {
@@ -14,23 +14,32 @@ contract Staking {
 
     uint256 MIN_STAKE = 0.1 ether;
     uint8 ERC20_AMOUNT_PER_DAY = 100;
+    uint32 DAY = 1 days;
+    uint32 MONTH = DAY * 30;
+    uint8 MAX_PERCENT = 50;
 
     mapping(address => Stake) public stakes;
+
+    event Unstake(uint256 value);
+    event UnstakeCommision(uint256 value);
 
     constructor(address _token) {
         ERC20Rewards = IERC20Rewards(_token);
     }
 
     function harvest() public {
-        require(stakes[msg.sender].value > 0, "Don't have yet active stake");
+        Stake storage activeStake = stakes[msg.sender];
 
-        uint256 rewardDays = (block.timestamp -
-            stakes[msg.sender].getRewardsTime) / 1 days;
+        uint256 rewardDays = (block.timestamp - activeStake.getRewardsTime) /
+            uint256(DAY);
 
-        uint256 rewards = uint256(ERC20_AMOUNT_PER_DAY) * rewardDays;
-
-        stakes[msg.sender].getRewardsTime = uint32(block.timestamp);
-        ERC20Rewards.mint(msg.sender, rewards);
+        if (rewardDays > 0) {
+            uint256 rewardPerDay = (activeStake.value / MIN_STAKE) *
+                uint256(ERC20_AMOUNT_PER_DAY);
+            uint256 rewards = rewardPerDay * rewardDays;
+            activeStake.getRewardsTime = uint32(block.timestamp);
+            ERC20Rewards.mint(msg.sender, rewards);
+        }
     }
 
     function stake() public payable {
@@ -44,8 +53,8 @@ contract Staking {
         );
 
         if (stakes[msg.sender].value > 0) {
-            stakes[msg.sender].value += msg.value;
             harvest();
+            stakes[msg.sender].value += msg.value;
         } else {
             Stake memory newStake = Stake(
                 msg.value,
@@ -53,6 +62,49 @@ contract Staking {
                 uint32(block.timestamp)
             );
             stakes[msg.sender] = newStake;
+        }
+    }
+
+    function unstake(uint256 _value) public payable {
+        Stake storage activeStake = stakes[msg.sender];
+        require(activeStake.value > 0, "User don't have active stake");
+        require(
+            _value <= activeStake.value,
+            "unstake value is greather than active stake value"
+        );
+
+        uint256 stakeSeconds = (block.timestamp - activeStake.stakeStartTime);
+        uint256 stakedDays = stakeSeconds / uint256(DAY);
+
+        uint256 unstakeAmount;
+        uint256 toStakeOwnerAmount;
+
+        if (stakedDays < 30) {
+            unstakeAmount = (_value * uint256(MAX_PERCENT)) / 100;
+            toStakeOwnerAmount = (_value * uint256(MAX_PERCENT)) / 100;
+        } else if (stakedDays >= 60) {
+            unstakeAmount = _value;
+        } else {
+            uint256 percent = ((stakeSeconds - uint256(MONTH)) *
+                uint256(MAX_PERCENT)) / uint256(MONTH);
+            toStakeOwnerAmount = (_value * percent) / 100;
+            unstakeAmount = _value - toStakeOwnerAmount;
+        }
+
+        require(unstakeAmount > 0, "unstake for zero amount");
+
+        harvest();
+
+        activeStake.value -= unstakeAmount + toStakeOwnerAmount;
+        activeStake.stakeStartTime = 0;
+
+        payable(msg.sender).transfer(unstakeAmount);
+
+        emit Unstake(unstakeAmount);
+
+        if (toStakeOwnerAmount > 0) {
+            payable(owner()).transfer(toStakeOwnerAmount);
+            emit UnstakeCommision(toStakeOwnerAmount);
         }
     }
 }
--- test/staking.js
+++ test/staking.js
@@ -1,10 +1,15 @@
 const Staking = artifacts.require('Staking');
 const ERC20Rewards = artifacts.require('ERC20Rewards');
-const { BN, time } = require('@openzeppelin/test-helpers');
 
-contract('Staking', ([alice]) => {
+const { BN, time, expectEvent } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+
+contract('Staking', ([alice, bob]) => {
   const ONE = new BN('1000000000000000000');
+  const TWO = new BN('2000000000000000000');
   const DAY = new BN(86400);
+  const FOURTY_FIVE_DAYS = new BN(3888000);
+  const TWO_MONTH = new BN(5184000);
 
   let stakingInstance;
   let ERC20RewardsInstance;
@@ -22,20 +27,109 @@ contract('Staking', ([alice]) => {
   });
 
   context('test staking', () => {
-    it('test', async () => {
+    it('should be able to stake', async () => {
       await stakingInstance.stake({
-        from: alice,
+        from: bob,
         value: ONE,
       });
 
-      time.increase(DAY);
+      const stake = await stakingInstance.stakes(bob, { from: bob });
 
-      await stakingInstance.harvest({ from: alice });
-      await stakingInstance.harvest({ from: alice });
-      const balance = await ERC20RewardsInstance.balanceOf(alice, {
-        from: alice,
+      expect(stake.value).bignumber.equal(ONE);
+    });
+
+    it('should be able to add ether to active stake', async () => {
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
       });
-      console.log(balance.toString(), 'balance');
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+
+      const stake = await stakingInstance.stakes(bob, { from: bob });
+
+      expect(stake.value).bignumber.equal(TWO);
+    });
+
+    it('should be able to get reward for 1 ether after add ether to active stake', async () => {
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+      const balanceBeforeHarvest = await ERC20RewardsInstance.balanceOf(bob, {
+        from: bob,
+      });
+      expect(balanceBeforeHarvest).bignumber.equal(new BN(0));
+      time.increase(DAY);
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+      const balanceAfterHarvest = await ERC20RewardsInstance.balanceOf(bob, {
+        from: bob,
+      });
+      expect(balanceAfterHarvest).bignumber.equal(new BN(1000));
+    });
+
+    it('should be able to unstake with 1 ether in active stake with 50% commision', async () => {
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+      const unstake = await stakingInstance.unstake(ONE, { from: bob });
+
+      expectEvent(unstake, 'Unstake', {
+        value: '500000000000000000',
+      });
+      expectEvent(unstake, 'UnstakeCommision', {
+        value: '500000000000000000',
+      });
+
+      const balanceERC20 = await ERC20RewardsInstance.balanceOf(bob, {
+        from: bob,
+      });
+      expect(balanceERC20).bignumber.equal(new BN(0));
+    });
+
+    it('should be able to unstake with 1 ether in active stake with 25% commision', async () => {
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+      time.increase(FOURTY_FIVE_DAYS);
+      const unstake = await stakingInstance.unstake(ONE, { from: bob });
+
+      expectEvent(unstake, 'Unstake', {
+        value: '750000000000000000',
+      });
+      expectEvent(unstake, 'UnstakeCommision', {
+        value: '250000000000000000',
+      });
+
+      const balanceERC20 = await ERC20RewardsInstance.balanceOf(bob, {
+        from: bob,
+      });
+      expect(balanceERC20).bignumber.equal(new BN(45000));
+    });
+
+    it('should be able to unstake with 1 ether in active stake with 0% commision', async () => {
+      await stakingInstance.stake({
+        from: bob,
+        value: ONE,
+      });
+      time.increase(TWO_MONTH);
+      const unstake = await stakingInstance.unstake(ONE, { from: bob });
+
+      expectEvent(unstake, 'Unstake', {
+        value: ONE,
+      });
+
+      const balanceERC20 = await ERC20RewardsInstance.balanceOf(bob, {
+        from: bob,
+      });
+      expect(balanceERC20).bignumber.equal(new BN(60000));
     });
   });
 });
